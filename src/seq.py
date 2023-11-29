8fb9d347af49f1a39ccee5f1fa07eb5d084c4de9
RyanMist7 <ryanhmistretta@gmail.com>
2023-11-29 13:42:25 -0500

    Finished seq.py

--- src/seq.py
+++ src/seq.py
@@ -4,32 +4,83 @@ from Bio.pairwise2 import format_alignment
 from Bio.Align import substitution_matrices
 import Ref.TP53Ref as TP53Ref
 import Ref.GATA3Ref as GATA3Ref
+import Ref.PIK3CARef as PIK3CARef
 
 
-def align(input):
+def get_max(input):
+    simple = True
+
     # Aligment Parameters
     matrix = substitution_matrices.load("BLOSUM62")
     gap_open = -10
     gap_extend = -0.5
 
-    max_align, max_gene = None, None
+    max_align, max_gene, max_res, ref = None, None, None, None
 
     # TP53 Gene Aligment
     align_TP53 = pairwise2.align.localds(
         TP53Ref.TP53Ref, input, match_dict=matrix, open=gap_open, extend=gap_extend
     )[0]
-    max_align, max_gene = align_TP53.score, "TP53"
+    max_align, max_gene, max_res = align_TP53.score, "TP53", align_TP53
+    ref = TP53Ref.TP53Ref
 
     # GATA3 Gene Aligment
     align_GATA3 = pairwise2.align.localds(
         GATA3Ref.GATA3Ref, input, match_dict=matrix, open=gap_open, extend=gap_extend
     )[0]
     if align_GATA3.score > max_align:
-        max_align, max_gene = align_GATA3.score, "GATA3"
+        max_align, max_gene, max_res = align_GATA3.score, "GATA3", align_GATA3
+        ref = GATA3Ref.GATA3Ref
 
-    print("Score: " + str(max_align))
-    print("Gene: " + max_gene)
-    return max_gene
+    if simple:
+        return (max_gene, max_res, ref)
 
-    # Utilize gene frequency (mult score by percentage) -> choose max ->
-    # Gene (TP53), Start, End, Ref, Var
+    # PIK3CA Gene Aligment
+    align_PIK3CA = pairwise2.align.localds(
+        PIK3CARef.PIK3CARef, input, match_dict=matrix, open=gap_open, extend=gap_extend
+    )[0]
+    if align_PIK3CA.score > max_align:
+        max_align, max_gene, max_res = align_PIK3CA.score, "PIK3CA", align_PIK3CA
+        ref = PIK3CARef.PIK3CARef
+
+    # TODO: still need to do CDH1
+
+    return (max_gene, max_res, ref)
+
+
+def align(input):
+    # TODO: Eventually we will utilize gene freq (mult score by percentage)
+    start_pos = {
+        "TP53": 7571720,
+        "GATA3": 8096667,
+        "PIK3CA": 178866311,
+        "CDH1": 68771195,
+    }
+
+    max_gene_name, max_res, ref = get_max(input)
+    result_sequence = max_res.seqB[max_res.start : max_res.end]
+
+    # Compares result sequence vs reference seqeuence
+    difference = []
+    for i in range(0, max_res.end - max_res.start):
+        ref_base = ref[i + max_res.start]
+        res_base = result_sequence[i]
+        if ref_base != res_base:
+            difference.append((ref_base, res_base))
+
+    # [Gene, Start, End, [(Ref, Var)]]
+    # Ex. ["TP53", 8106058, 8106058, [(T, A)]]
+    res = [
+        max_gene_name,
+        max_res.start + start_pos[max_gene_name],
+        max_res.end + start_pos[max_gene_name],
+        difference,
+    ]
+
+    print("Res Sequence: " + result_sequence)
+    print("Ref Sequence: " + ref[max_res.start : max_res.end])
+    print("Result Vector: ")
+    print(res)
+    print()
+
+    return res
--- src/test.py
+++ src/test.py
@@ -1,18 +1,27 @@
 import unittest
 import seq
 
+# Run Using python src/test.py
+
 
 class TestAlign(unittest.TestCase):
-    def test_basic_GATA3(self):
+    def test_identity_GATA3(self):
         GATA3_input1 = "CTGAAGAACATGAATTTCTCAAATCAAAGTAAAGCTGAGGAATCCTCTCCATGGCTCACTTCTACCTCCTCAATCTTAATGAGCTT"
         GATA3_input2 = (
             "GAGGTTGCAGTGAGCTGAGATCGCACCACTGCACTCCAGCCTGGGTGACAGAGCGAGACTCCATCT"
         )
         GATA3_input3 = "CCCTACTACGGAAACTCGGTCAGGGCCACGGTGCAGAGGTACCCTCCGACCCACCACGGTGAGTGCGCCCGGGGTGCCGGGGCTCCCGCCGGCCGCTTCAGCCGTCCCGGCTCGGGGAGGTCGGGAGG"
 
-        self.assertEqual(seq.align(GATA3_input1), "GATA3")
-        self.assertEqual(seq.align(GATA3_input2), "GATA3")
-        self.assertEqual(seq.align(GATA3_input3), "GATA3")
+        self.assertEqual(seq.align(GATA3_input1), ["GATA3", 8113468, 8113554, []])
+        self.assertEqual(seq.align(GATA3_input2), ["GATA3", 8110649, 8110715, []])
+        self.assertEqual(seq.align(GATA3_input3), ["GATA3", 8097802, 8097930, []])
+
+    def test_change_GATA3(self):
+        GATA3_input1 = "CAGAAGAACATGAATTTCTCAAATCAAAGTAAAGCTGAGGAATCCTCTCCATGGCTCACTTCTACCTCCTCAATCTTAATGAGCTT"
+
+        self.assertEqual(
+            seq.align(GATA3_input1), ["GATA3", 8113468, 8113554, [("T", "A")]]
+        )
 
 
 # Running the tests
