7fa9e69b0e5df8dfd3bddb3fed2203142e3a26d0
Paul Garell <prgarell@gmail.com>
2023-12-15 12:44:05 -0500

    Update images

--- src/preProcess.py
+++ src/preProcess.py
@@ -5,17 +5,27 @@ df_GATA3 = pd.read_csv('data/GATA3.csv')
 df_PIK3CA = pd.read_csv('data/PIK3CA.csv')
 df_TP53 = pd.read_csv('data/TP53.csv')
 
-print(df_CDH1)
-print("******************************")
-print(df_GATA3)
-print("******************************")
-print(df_PIK3CA)
-print("*********/*********************")
-print(df_TP53)
+# print(df_CDH1)
+# print("******************************")
+# print(df_GATA3)
+# print("******************************")
+# print(df_PIK3CA)
+# print("*********/*********************")
+# print(df_TP53)
 
-# df2 = pd.DataFrame(df_CDH1["Mutation Type"] != "Missense_Mutation")
+
+labl = "Mutation Type"
+toSelect = df_CDH1.columns.to_list()
+toSelect.remove(labl)
+toSelect.remove("Reference")
+
+# df2 = df_CDH1.loc[:, df_CDH1.columns != labl and df_CDH1.columns != "Reference"]
+df2 = df_CDH1[toSelect]
+
+# df2 = df_CDH1[labl]
+
 # df2 = df_CDH1.loc[df_CDH1["Mutation Type"] != "Missense_Mutation"]
 
 # st = len(df_CDH1["Reference"][0])
 
-# print(st)
+print(df2)
