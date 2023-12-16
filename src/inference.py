4cc189f9d6a31dee300da48b2245fbcc89106eea
RyanMist7 <ryanhmistretta@gmail.com>
2023-12-15 21:47:15 -0500

    Sequence Align in Inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,11 +1,10 @@
+from seq import align
 
-
-#import Ryan function:
-
-#Ryan function provided a gene sequence and outputs a 
+# import Ryan function:
+input = ""
+output = align(input)
 # list of [Chromosome, Start Pos, End pos, Ref, Var, Gene name]
-#for example [17,7578190, 7578190.0, T, C, 0.26, TP53]
+# for example [17,7578190, 7578190.0, T, C, 0.26, TP53]
 
 
-#Paul inferences on model and outputs predicted cancer type probabilities 
-
+# Paul inferences on model and outputs predicted cancer type probabilities
--- src/seq.py
+++ src/seq.py
@@ -5,6 +5,7 @@ from Bio.Align import substitution_matrices
 import Ref.TP53Ref as TP53Ref
 import Ref.GATA3Ref as GATA3Ref
 import Ref.PIK3CARef as PIK3CARef
+import Ref.CDH1Ref as CDH1Ref
 
 
 def get_max(input):
@@ -13,13 +14,13 @@ def get_max(input):
     gap_open = -10
     gap_extend = -0.5
 
-    # TODO: still need to do CDH1
     max_align, max_gene, max_res, ref = -float("inf"), None, None, None
 
     gene_ref = {
         "TP53": TP53Ref.TP53Ref,
         "GATA3": GATA3Ref.GATA3Ref,
-        "PIK3CA": PIK3CARef.PIK3CARef,
+        # "PIK3CA": PIK3CARef.PIK3CARef,
+        # "CDH1": CDH1Ref.CDH1Ref,
     }
 
     for gene in gene_ref:
@@ -38,12 +39,11 @@ def get_max(input):
 
 
 def align(input):
-    # TODO: Eventually we will utilize gene freq (mult score by percentage)
-    start_pos = {
-        "TP53": 7571720,
-        "GATA3": 8096667,
-        "PIK3CA": 178866311,
-        "CDH1": 68771195,
+    info = {
+        "TP53": [7571720, 17],
+        "GATA3": [8096667, 10],
+        "PIK3CA": [178866311, 3],
+        "CDH1": [68771195, 16],
     }
 
     max_gene_name, max_res, ref = get_max(input)
@@ -55,19 +55,22 @@ def align(input):
         ref_base = ref[i + max_res.start]
         res_base = result_sequence[i]
         if ref_base != res_base:
-            difference.append((ref_base, res_base))
+            difference.append(
+                [ref_base, res_base, i + max_res.start + info[max_gene_name][0]]
+            )
 
-    res = [
-        max_gene_name,
-        max_res.start + start_pos[max_gene_name],
-        max_res.end + start_pos[max_gene_name],
-        difference,
-    ]
-
-    print("Res Sequence: " + result_sequence)
-    print("Ref Sequence: " + ref[max_res.start : max_res.end])
-    print("Result Vector: ")
+    res = []
+    for dif in difference:
+        res.append(
+            [
+                info[max_gene_name][1],  # chromosome
+                dif[2],  # start
+                dif[2],  # end
+                dif[0],  # ref
+                dif[1],  # variation
+                max_gene_name,  # name
+            ]
+        )
     print(res)
-    print()
 
     return res
