90fc5aa63e6e156de8a9113b439a34d6d7ff02d6
Paul Garell <prgarell@gmail.com>
2023-12-15 21:34:24 -0500

    Start inference

---
+++ src/inference.py
@@ -0,0 +1,11 @@
+
+
+#import Ryan function:
+
+#Ryan function provided a gene sequence and outputs a 
+# list of [Chromosome, Start Pos, End pos, Ref, Var, Gene name]
+#for example [17,7578190, 7578190.0, T, C, 0.26, TP53]
+
+
+#Paul inferences on model and outputs predicted cancer type probabilities 
+
